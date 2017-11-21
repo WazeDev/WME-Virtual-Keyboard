4c631c2701ebf56a52ab6c1735ac18b06f42ab6c
mapomatic <mapomatic.waze@gmail.com>
2017-11-20 23:13:51 -0500

    Overhaul to just use a static div
    Will work on more advanced presentation stuff later...

--- WME-Virtual-Keyboard.js
+++ WME-Virtual-Keyboard.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Virtual Keyboard
 // @namespace    http://tampermonkey.net/
-// @version      2017.11.20.002
+// @version      2017.11.20.003
 // @description  Adds a "virtual keyboard" to enter non-english characters (currently only Turkish characters).
 // @author       MapOMatic
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -11,20 +11,6 @@
 
 (function() {
     'use strict';
-
-    function hidePopup($popup, $parentMenu) {
-        var timeoutId = setTimeout(function() {
-            $popup.css({display:'none'});
-            $parentMenu.css({'border-radius':'8px 8px 8px 8px', 'background-color':'#999', 'box-shadow':''});
-        }, 100);
-        $popup.data('timeoutId', timeoutId);
-    }
-
-    function showPopup($popup, $parentMenu) {
-        $popup.css({display:'inherit'});
-        $parentMenu.css({'border-radius':'8px 8px 0px 0px', 'background-color':'#777', 'box-shadow':'#b2b2b2 2px 2px 8px'});
-        clearTimeout($popup.data('timeoutId'));
-    }
 
     function buttonOnMouseDown(evt) {
         evt.preventDefault();
@@ -48,52 +34,30 @@
 
         var $menuParentDiv = $('#advanced-tools');
 
-        var $menuDiv = $('<div>', {id:'more-letters-menu'})
-        .css({width:'56px', height:'18px', left:'10px', top:'27px', 'background-color': '#999', 'padding-top':'1px',
-              'z-index':'2000', 'margin-top':'-12px', 'margin-bottom':'2px', cursor:'default', 'font-size':'12px',
-              'text-align':'center', color:'white', 'border-radius':'8px 8px 8px 8px'})
-        .text('Turkish')
+        var $keyboardDiv = $('<div>', {id:'more-letters-popup'})
+        .css({padding:'2px', 'background-color': '#ccc',
+              'box-shadow':'#b2b2b2 2px 2px 8px', 'z-index':'2001', cursor:'default', 'font-size':'13px',
+              'text-align':'center', color:'black', 'margin-bottom':'4px', 'border-radius':'8px'})
         .appendTo($menuParentDiv);
 
-        var offset = $menuDiv.position();
-
-        var $popupDiv = $('<div>', {id:'more-letters-popup'})
-        .css({padding:'2px', left:offset.left, top:offset.top + $menuDiv.height(), 'background-color': '#f8f8f8',
-              'box-shadow':'#b2b2b2 2px 2px 8px', 'z-index':'2001', 'margin-top':'-11px', cursor:'default', 'font-size':'13px',
-              'text-align':'center', color:'black', display:'none', position:'absolute'})
-        .appendTo($menuParentDiv);
-
-        var chars = [['Ç','ç'],['Ğ','ğ'],['I','ı'],['İ','i'],['Ö','ö'],['Ş','ş'],['Ü','ü']];
+        var chars = [['Ç','Ğ','I','İ','Ö','Ş','Ü'],['ç','ğ','ı','i','ö','ş','ü']];
         var $table = $('<table>');
         var buttonStyle = {width:'22px',height:'22px',padding:'unset'};
-        var tdStyle = {padding:'4px 4px'};
-        chars.forEach(function(pair) {
-            $table.append($('<tr>').append(
-                $('<td>').css(tdStyle).append($('<button>', {class:'waze-btn waze-btn-white'}).text(pair[0]).css(buttonStyle).mousedown(buttonOnMouseDown)),
-                $('<td>').css(tdStyle).append($('<button>', {class:'waze-btn waze-btn-white'}).text(pair[1]).css(buttonStyle).mousedown(buttonOnMouseDown))
-            ));
+        var tdStyle = {padding:'2px 4px'};
+        chars.forEach(function(row) {
+            var $row = $('<tr>');
+            row.forEach(function(char) {
+                $row.append(
+                    $('<td>').css(tdStyle).append($('<button>', {class:'waze-btn waze-btn-white'}).text(char).css(buttonStyle).mousedown(buttonOnMouseDown))
+                );
+            });
+            $table.append($row);
         });
-        $popupDiv.append($table);
+        $keyboardDiv.append($table);
 
-        $menuDiv.mousedown(function(evt) {
+        $keyboardDiv.mousedown(function(evt) {
             evt.preventDefault();
-            showPopup($popupDiv, $menuDiv);
-            clearTimeout($menuDiv.data('timeoutId'));
-        }).mouseenter(function() {
-            var timeoutId = setTimeout(function() {
-                showPopup($popupDiv, $menuDiv);
-            }, 100);
-            $menuDiv.data('timeoutId', timeoutId);
-        }).mouseleave(function() {
-            hidePopup($popupDiv, $menuDiv);
-            clearTimeout($menuDiv.data('timeoutId'));
-        });
-
-        $popupDiv.mouseleave(function() {
-            hidePopup($popupDiv, $menuDiv);
-        }).mouseenter(function() {
-            showPopup($popupDiv, $menuDiv);
-        }).mousedown(function(evt) {
+        }).click(function(evt) {
             evt.preventDefault();
         });
     }
